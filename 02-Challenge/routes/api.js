124dfe0c212fd363e664a198cda1c4999a26e9c8
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-04-09 13:54:27 -0700

    added base code for get and post -Notes

---
+++ 02-Challenge/routes/api.js
@@ -0,0 +1,20 @@
+// GET /notes read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", function(req, res) {
+    fs.readFile("db/db.json", "utf8", function(err, data) {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+})
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+app.post("/api/notes", function(req, res) {
+    fs.readFile("db/db.json", "utf8", function(err, data) {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+        notes.push(req.body);
+        fs.writeFile("db/db.json", JSON.stringify(notes), function(err) {
+            if (err) throw err;
+            res.json(notes);
+        });
+    });
+})
