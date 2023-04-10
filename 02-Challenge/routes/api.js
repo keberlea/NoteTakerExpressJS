b570b93ef40ceee6c4a6a3bcb06f4ae9d6b5c503
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-04-10 13:18:55 -0700

    added server code

--- 02-Challenge/routes/api.js
+++ 02-Challenge/routes/api.js
@@ -1,7 +1,12 @@
 //require express, fs, and id-16
-const express = require("express");
+const apiNotes = require("express").Router();
 const fs = require("fs");
 const id = require("id-16");
+
+//create id generator
+let uid = id_16.generator(5);
+
+
 
 // GET /notes read the db.json file and return all saved notes as JSON.
 app.get("/api/notes",  function(req, res) {
@@ -13,6 +18,12 @@ app.get("/api/notes",  function(req, res) {
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 app.post("/api/notes", function(req, res) {
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uid(),
+        
+    }
     fs.readFile("db/db.json", "utf8", function(err, data) {
         if (err) throw err;
         var notes = JSON.parse(data);
