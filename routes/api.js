ab27fdc2c51e97da5a12bc92d5f24a192cdd6933
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-04-11 20:04:15 -0700

    troubleshooting to resolve routing issues

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,20 @@
 //require express, fs, and id-16
 const apiNotes = require('express').Router();
 const fs = require('fs');
-const id_16 = require('id-16');
+const  id_16 = require('id-16');
 
-//create id generator
+//create id generator that generates a random id with a set length in ()
 let uid = id_16.generator(5);
-
-
-
 
 
 // GET /notes read the db.json file and return all saved notes as JSON.
 apiNotes.get("/api/notes", (req, res) => {
-    let data = JSON.parse(fs.readFile("./db/db.json", "utf8"))
-});
+    const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
+    res.json(data)
+})
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
-apiNotes.post("/api/notes", (req, res) => {
+apiNotes.post('/api/notes', (req, res) => {
     let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
     const newNote = {
         title: req.body.title,
@@ -24,8 +22,11 @@ apiNotes.post("/api/notes", (req, res) => {
         id: uid(), 
     };
     data.push(newNote);
-    fs.writeFile("db/db.json", JSON.stringify(data))
-    });
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(data))
+
+    res.json(data)
+    })
 
     //function to dele note from db.json file
     //apiNotes.delete("/api/notes/:id", (req, res) => {
