f6a9afacf7d1bf87d11b4dfb95f0c86ab33c1c35
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-04-13 14:35:26 -0700

    used different id generator and id is being console logged

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,28 @@
 //require express, fs, and id-16
 const apiNotes = require('express').Router()
 const fs = require('fs')
-const id_16 = require('id-16')
-const path = require("path")
+//const id_16 = require('id-16')
+
+const generateUniqueId = require('generate-unique-id');
+const id = generateUniqueId({
+    length: 32,
+    useLetters: false
+});
+
+console.log (id)
 
 
 //create id generator that generates a random id with a set length in ()
-let uid = id_16.generator(5);
+//let uid = id_16.generator(5);
 
-console.log(uid)
+//console.log(uid)
+
+
 
 
 // GET /api/notes read the db.json file and return all saved notes as JSON.
 apiNotes.get('/api/notes', async (req, res) => {
-    const data = await JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
+    let data = await JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
     console.log(JSON.stringify(data))
     res.json(data)
 })
@@ -21,14 +30,11 @@ apiNotes.get('/api/notes', async (req, res) => {
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 apiNotes.post('/api/notes', (req, res) => {
     let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
-    console.log(data)
-
     const newNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uid(), 
+        title: req.params.title,
+        text: req.params.text,
+        id: id(), 
     };
-    console.log(newNote)
 
     data.push(newNote)
     
@@ -40,14 +46,14 @@ apiNotes.post('/api/notes', (req, res) => {
 
 
 //function to delete note from db.json file
-/*
+
 apiNotes.delete("/api/notes/:id", (req, res) => {
     const deleteID = req.params.id.toString()
     const data = JSON.parse(fs.readFileSync('db/db.json','utf8'))
     const newData = data.filter(note => note.id.toString() !== deleteID)
-    fs.writeFileSync('db/db.json', JSON.stringify(newData))
+    fs.writeFileSync('./db/db.json', JSON.stringify(newData))
     res.json(newData)
 })
-*/
+
 
 module.exports = apiNotes
